6e9b8eaa4267b39fbba174df7c5ae8feec1b7b39
Rudihmar <rudihmar@gmail.com>
2021-09-02 20:57:05 -0400

    test

--- src/components/TemplateInfo.jsx
+++ src/components/TemplateInfo.jsx
@@ -7,7 +7,7 @@ const TemplateInfo = () => {
     
       <div className="template">
 
-          <div className="titulo"><p>TITULO</p></div>
+          <div className="titulo"><p>NOMBRE</p></div>
           <div className="parrafo"><p>DESCRIPCION</p></div>
           <div className="imagen"><p>IMAGEN</p></div>
           <div className="label"><p>LABEL</p></div>
